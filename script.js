044135ea13e98ca3b57a95f138b21cd6c26de8fa
Glitch (glitch-blank-website) <none>
2022-09-01 09:21:58 +0000

    👗😀 Checkpoint ./script.js:48224736/1723 ./index.html:48224736/11

--- script.js
+++ script.js
@@ -7,3 +7,14 @@ bar.addEventListener("click", () => {
 close.addEventListener("click", () => {
   menu.classList.remove("active");
 });
+const form = document.querySelector("#form");
+const name = document.querySelector('input[type="text"]');
+const email = document.querySelector('input[type="email"]');
+const message = document.querySelector("#contact-message");
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  alert("Project Sumitted");
+  name.value = "";
+  email.value = "";
+  message.value = "";
+});
