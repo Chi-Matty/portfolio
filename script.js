f28b324132ae23ccc4303ca6b500ef346ff54d46
Glitch (glitch-blank-website) <none>
2022-09-03 12:09:38 +0000

    🌯⛸ Checkpoint ./script.js:48224736/8003 ./index.html:48224736/29 ./style.css:48224736/31316

--- script.js
+++ script.js
@@ -13,8 +13,74 @@ const email = document.querySelector('input[type="email"]');
 const message = document.querySelector("#contact-message");
 form.addEventListener("submit", function (e) {
   e.preventDefault();
-  alert("Project Sumitted");
+  alert("Project Submitted");
   name.value = "";
   email.value = "";
   message.value = "";
+});
+
+window.addEventListener("scroll", () => {
+  let content = document.querySelector(".first-scroll");
+  let contentPosition = content.getBoundingClientRect().top;
+  contentScreen = window.innerHeight / 0.9;
+  if (contentPosition < contentScreen) {
+    content.classList.add("scroll");
+  } else {
+    content.classList.remove("scroll");
+  }
+});
+
+window.addEventListener("scroll", () => {
+  let content = document.querySelector(".second-scroll");
+  let contentPosition = content.getBoundingClientRect().top;
+  contentScreen = window.innerHeight / 0.9;
+  if (contentPosition < contentScreen) {
+    content.classList.add("scroll");
+  } else {
+    content.classList.remove("scroll");
+  }
+});
+
+window.addEventListener("scroll", () => {
+  let content = document.querySelector(".third-scroll");
+  let contentPosition = content.getBoundingClientRect().top;
+  contentScreen = window.innerHeight / 0.9;
+  if (contentPosition < contentScreen) {
+    content.classList.add("scroll");
+  } else {
+    content.classList.remove("scroll");
+  }
+});
+
+window.addEventListener("scroll", () => {
+  let content = document.querySelector(".fourth-scroll");
+  let contentPosition = content.getBoundingClientRect().top;
+  contentScreen = window.innerHeight / 1.1;
+  if (contentPosition < contentScreen) {
+    content.classList.add("for-scroll");
+  } else {
+    content.classList.remove("for-scroll");
+  }
+});
+
+window.addEventListener("scroll", () => {
+  let content = document.querySelector(".fifth-scroll");
+  let contentPosition = content.getBoundingClientRect().top;
+  contentScreen = window.innerHeight / 0.9;
+  if (contentPosition < contentScreen) {
+    content.classList.add("scroll");
+  } else {
+    content.classList.remove("scroll");
+  }
+});
+
+window.addEventListener("scroll", () => {
+  let content = document.querySelector(".sixth-scroll");
+  let contentPosition = content.getBoundingClientRect().top;
+  contentScreen = window.innerHeight / 0.9;
+  if (contentPosition < contentScreen) {
+    content.classList.add("scroll");
+  } else {
+    content.classList.remove("scroll");
+  }
 });
