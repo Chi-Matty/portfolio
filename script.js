c46d6d0e0b0ae2590b498df600e01c02b2cd4e39
Glitch (glitch-blank-website) <none>
2024-01-17 07:55:58 +0000

    😼📯 Checkpoint ./index.html:48224736/74 ./style.css:48224736/63 ./script.js:48224736/4174

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
 const bar = document.querySelector(".bar");
 const menu = document.querySelector(".menu");
-const close = document.querySelector(".close");
 bar.addEventListener("click", () => {
-  menu.classList.add("active");
-});
-close.addEventListener("click", () => {
-  menu.classList.remove("active");
+  menu.classList.toggle("active");
 });
 
 // SCROLL VIEW
