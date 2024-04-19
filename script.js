245d7db5061724dcc6bf91ca5ec317cef7256a66
Glitch (glitch-blank-website) <none>
2024-04-19 23:38:19 +0000

    🏬🐞 Checkpoint ./script.js:48224736/7694

--- script.js
+++ script.js
@@ -100,3 +100,18 @@ document
     // Open default email client with mailto link
     window.location.href = mailtoLink;
   });
+
+document.addEventListener("DOMContentLoaded", function () {
+  var links = document.querySelectorAll(".navbar-nav .nav-link");
+
+  links.forEach(function (link) {
+    link.addEventListener("click", function () {
+      // Close the navbar menu when a link is clicked
+      var navbarCollapse = document.querySelector(".navbar-collapse");
+      var bootstrapNavbar = new bootstrap.Collapse(navbarCollapse, {
+        toggle: false,
+      });
+      bootstrapNavbar.hide();
+    });
+  });
+});
